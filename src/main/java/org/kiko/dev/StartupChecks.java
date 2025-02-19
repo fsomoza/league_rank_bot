2e6be38ed65ffdfbfda8b9bc28ce8c8627ba80e5
kiko <kikosomotri@gmail.com>
2025-02-20 00:51:20 +0100

    added todo task

--- src/main/java/org/kiko/dev/StartupChecks.java
+++ src/main/java/org/kiko/dev/StartupChecks.java
@@ -16,48 +16,51 @@ import java.net.URLConnection;
 
 public class StartupChecks {
 
-    private final MongoDbAdapter mongoDbAdapter;
+   private final MongoDbAdapter mongoDbAdapter;
 
-    public StartupChecks(){
-        this.mongoDbAdapter = MongoDbAdapter.getInstance();
+  public StartupChecks() {
+    this.mongoDbAdapter = MongoDbAdapter.getInstance();
+  }
+
+  public void doChecks() {
+    // go to riot version url and retrieve the las version
+
+    String urlString = "https://ddragon.leagueoflegends.com/api/versions.json";
+
+    try {
+      OkHttpClient client = new OkHttpClient();
+      Response response = client.newCall(new Request.Builder().url(urlString).build()).execute();
+      if (!response.isSuccessful()) {
+        throw new IOException("error retrieving the vesion json");
+      }
+
+      String responseBody = response.body().string();
+      ObjectMapper objectMapper = new ObjectMapper();
+      JsonNode rootNode = objectMapper.readTree(responseBody);
+
+      if (!rootNode.isArray()) {
+        throw new Exception("response type of versions is not the expected one");
+      }
+      // get the first element of the array which is the last version
+      String version = rootNode.get(0).toString();
+
+      // TODO Check if the version matches the one on the DB, if not, proceed to look
+      // for a new champ addition and if theres a
+      // new one, add it to the champions collection
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    public void doChecks() {
-        //go to riot version url and retrieve the las version
-
-        String urlString = "https://ddragon.leagueoflegends.com/api/versions.json";
-
-        try {
-            OkHttpClient client = new OkHttpClient();
-            Response response = client.newCall(new Request.Builder().url(urlString).build()).execute();
-            if (!response.isSuccessful()){
-                throw new IOException("error retrieving the vesion json");
-            }
-
-            String responseBody = response.body().string();
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootNode = objectMapper.readTree(responseBody);
-
-            if (!rootNode.isArray()){
-                throw new Exception("response type of versions is not the expected one");
-            }
-            // get the first element of the array which is the last version
-            String version = rootNode.get(0).toString();
-
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-//        String urlString = "https://ddragon.leagueoflegends.com/cdn/15.2.1/img/champion/" + championName + ".png";
-//        URL url = new URL(urlString);
-//
-//        // Open connection with timeout settings
-//        URLConnection connection = url.openConnection();
-//        connection.setConnectTimeout(5000);
-//        connection.setReadTimeout(5000);
-    }
+    // String urlString =
+    // "https://ddragon.leagueoflegends.com/cdn/15.2.1/img/champion/" + championName
+    // + ".png";
+    // URL url = new URL(urlString);
+    //
+    // // Open connection with timeout settings
+    // URLConnection connection = url.openConnection();
+    // connection.setConnectTimeout(5000);
+    // connection.setReadTimeout(5000);
+  }
 
 }
